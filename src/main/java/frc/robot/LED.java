cb56807467fcc98c7c5408d7f51eee0017c27cb0
Adam Baumbach <madeyeadam@gmail.com>
2026-05-19 16:22:52 -0400

    fixed joey led

--- src/main/java/frc/robot/LED.java
+++ src/main/java/frc/robot/LED.java
@@ -8,11 +8,22 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
+
 public class LED {
-    //Addressable Led 1 can be changed, it is the port #
-    LED = new AddressableLED(1);
-    LEDBuffer = new AddressableLEDBuffer(60);
-    LED.setlength(LEDSBuffer.getlength());
-    LEDPattern blue = LEDPattern.solid(Color.kBlue);
-    blue.applyTo(LEDBuffer);
+    // you forgot to add the specifiers here
+    // private/public VariableType variableName = new VariableType();
+    // we also typically use final variables at the top of the class for things that get changed manually, like the LED port here
+    private final int LED_PORT = 1;
+    private AddressableLED LED = new AddressableLED(LED_PORT);
+    private AddressableLEDBuffer LEDBuffer = new AddressableLEDBuffer(60);
+
+    // moved the pattern you had here so it can be stored for easier access
+    // i would probably try to put most patterns up here though you don't have to
+    private final LEDPattern disabledPattern = LEDPattern.solid(Color.kBlue);
+
+    // the other code you had was outside of a constructor or other function so it didn't like that
+    public LED() {
+        LED.setLength(LEDBuffer.getLength());
+        disabledPattern.applyTo(LEDBuffer);
+    }
 }
